b3b9017e3f29b6cec0d0d38b1615397fbfb607bb
unknown <gokussjindia@gmail.com>
2024-03-21 00:33:22 +0530

    Implementation of Monolithic nodejs basic architecture to connect mongoDB single instance setup

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config({ path: __dirname + ".env" });
+
+const express = require("express");
+const app = express(),
+  cors = require("cors"),
+  http = require("http"),
+  bodyParser = require("body-parser");
+global.mongoose = require("mongoose");
+
+// create connection with mongoDB server
+require("./mongo.config");
+
+// CORS Options
+const corsOptions = {
+  origin: "*",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: [
+    "X-Requested-With",
+    "Content-type",
+    "Authorization",
+    "token",
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+const port = process.env.PORT || 3000;
+
+// create http server
+const server = http.createServer(app);
+module.exports = server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
